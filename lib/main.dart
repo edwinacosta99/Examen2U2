4c560d341f9aa3aa4bde8953ba55f599d15517a5
Edwin Acosta <edwin.acosta@unah.hn>
2024-07-27 13:51:30 -0600

    examen

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,79 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App Examen',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: const Text('Examen - Edwin Acosta'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Haz una pregunta'),
-          ),
+      home: YesNoPage(),
+    );
+}
+}
+
+class YesNoPage extends StatefulWidget {
+  @override
+  _YesNoPageState createState() => _YesNoPageState();
+}
+
+class _YesNoPageState extends State<YesNoPage> {
+  TextEditingController _controller = TextEditingController();
+  String _responseText = "";
+  String _imageUrl = "";
+
+  void _fetchYesNoResponse(String question) async {
+    final response = await http.get(Uri.parse('https://yesno.wtf/api'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        _responseText = data['answer'];
+        _imageUrl = data['image'];
+        _controller.clear();
+      });
+    } else {
+      setState(() {
+        _responseText = "Failed to get response";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Yes/No API')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    if (_controller.text.endsWith('?')) {
+                      _fetchYesNoResponse(_controller.text);
+                    }
+                  },
+                ),
+                hintText: 'Enter your question',
+              ),
+            ),
+            SizedBox(height: 20),
+            _imageUrl.isNotEmpty
+                ? Image.network(_imageUrl)
+                : Container(),
+            SizedBox(height: 20),
+            Text(
+              _responseText,
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
         ),
       ),
     );
